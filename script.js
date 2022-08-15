e498a93257ac8eb7a35cd02ac3fbbaf295dd3460
amansharma612 <amansharma6122002@gmail.com>
2022-08-15 19:33:29 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,46 @@
 var toggler = document.getElementById('menu');
 var navbar = document.getElementsByClassName('nav-links');
+var form = document.getElementById("hobby-form");
+const formData = new FormData(form);
 toggler.onclick = function(){
     document.getElementById('dash-1').classList.toggle('dash1-move');
     document.getElementById('dash-3').classList.toggle('dash3-move');
 
     navbar[0].classList.toggle("mobile-nav");
 }
+
+
+form.addEventListener("submit" ,(e) => {
+    e.preventDefault();
+
+    var hobbies_selected = "";
+
+    for(const element of form.elements['hob-values']){
+        if(element.checked){
+            hobbies_selected = hobbies_selected + "\n " + element.value;
+        }
+        
+    }
+
+    
+
+    form.classList.add('display-disabler');
+
+
+    if(confirm("Hobbies selected -->" + hobbies_selected + "\n Want to Continue?")){
+    
+    for (const element of form.elements['hob-values']){
+            document.getElementById(element.value).classList.remove('display-enabler')
+            document.getElementById(element.value).classList.remove('display-disabler')
+            if(element.checked){
+                document.getElementById(element.value).classList.add('display-enabler')
+            } else {
+                document.getElementById(element.value).classList.add('display-disabler')
+            }
+        }
+    }
+    
+  
+
+})
 
